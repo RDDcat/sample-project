1763f7f2125c970c1ca404947a3cb73ca4378ddf
Maro <55569476+RDDcat@users.noreply.github.com>
2025-06-29 12:08:51 +0900

    업ㄷ제이터

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
